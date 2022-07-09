32a3415c499494967f3520c55e39b72f3a7c553d
BooleanBean <booleanbean@gmail.com>
2022-07-09 14:28:58 +0300

    revert: vercel logo in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
-import * as React from 'react';
+import * as React from "react";
 
-import Layout from '#/components/layout/Layout';
-import ArrowLink from '#/components/links/ArrowLink';
-import Seo from '#/components/Seo';
-import Vercel from '~/svg/Vercel.svg';
+import Layout from "#/components/layout/Layout";
+import ArrowLink from "#/components/links/ArrowLink";
+import Seo from "#/components/Seo";
 
 export default function HomePage() {
   return (
@@ -14,10 +13,7 @@ export default function HomePage() {
       <main>
         <section className='bg-white'>
           <div className='layout flex min-h-screen flex-col items-center justify-center text-center'>
-            <Vercel className='text-5xl' />
-            <h1 className='mt-4'>
-             Discord Bot Dashboard
-            </h1>
+            <h1>Discord Bot Dashboard</h1>
             <p className='mt-2 text-sm text-gray-800'>
               An open source discord bot dashboard with TailwindCSS.
             </p>
