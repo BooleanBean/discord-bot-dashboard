a1fa84173389879c85ff09d101303569281753f2
BooleanBean <booleanbean@gmail.com>
2022-07-13 12:08:48 +0300

    refactor: implement new layout system

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,25 +5,33 @@ import Layout from "#/components/layouts/Layout";
 import ArrowLink from "#/components/links/ArrowLink";
 import Seo from "#/components/Seo";
 
-export default function NotFoundPage() {
-  return (
-    <Layout>
-      <Seo templateTitle="Not Found" />
+import { NextPageWithLayout } from "#/global-types";
 
-      <main>
-        <section>
-          <div className="layout flex min-h-screen flex-col items-center justify-center text-center">
-            <RiAlarmWarningFill
-              size={60}
-              className="drop-shadow-glow animate-flicker text-red-500"
-            />
-            <h1 className="mt-8 text-4xl md:text-6xl">Page Not Found</h1>
-            <ArrowLink className="mt-4 md:text-lg" href="/">
-              Back to Home
-            </ArrowLink>
-          </div>
-        </section>
-      </main>
-    </Layout>
+const NotFound: NextPageWithLayout = () => {
+  return (
+    <main>
+      <section>
+        <div className="layout flex min-h-screen flex-col items-center justify-center text-center">
+          <RiAlarmWarningFill
+            size={60}
+            className="drop-shadow-glow animate-flicker text-red-500"
+          />
+          <h1 className="mt-8 text-4xl md:text-6xl">Page Not Found</h1>
+          <ArrowLink className="mt-4 md:text-lg" href="/">
+            Back to Home
+          </ArrowLink>
+        </div>
+      </section>
+    </main>
   );
-}
+};
+
+NotFound.getLayout = (page) => (
+  <Layout>
+    <Seo templateTitle="Not Found" />
+
+    {page}
+  </Layout>
+);
+
+export default NotFound;
